338937d3e0ff24926a30d44dca45ce928ceeaa35
joddiy <joddiy@qq.com>
2018-12-13 22:16:24 +0800

    m

---
+++ commands/SsController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * author:       joddiyzhang <joddiyzhang@gmail.com>
+ * createTime:   2018-12-13 22:02
+ * fileName :    SsController.php
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+class SsController extends Controller
+{
+    public function actionCreateUser()
+    {
+        $port = 0;
+
+    }
+}
